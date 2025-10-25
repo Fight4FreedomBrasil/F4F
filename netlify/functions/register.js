15609b5153b044c352c96082c3658d1312c448e0
Chrysthian <cytchrisley@gmail.com>
2025-10-25 00:06:38 -0300

    add Netlify

---
+++ netlify/functions/register.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+    
+    const response = await fetch('https://script.google.com/macros/s/AKfycby6qmRd8uq-t95tMqE6Ed2f-HFq1tb2LMzo3A8lIO-CNHnLa1urc5RlyB_-GktZXSVN/exec', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(result)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ success: false, error: error.message })
+    };
+  }
+};
